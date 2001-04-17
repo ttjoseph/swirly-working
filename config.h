eef7d17ddefd027794d909fcc5252beeedf179f9
Tom Joseph <ttjoseph@gmail.com>
2001-04-17 20:32:32 +0000

    Initial revision

---
+++ config.h
@@ -0,0 +1,18 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the <GL/gl.h> header file.  */
+#define HAVE_GL_GL_H 1
+
+/* Define if you have the <SDL/SDL.h> header file.  */
+#define HAVE_SDL_SDL_H 1
+
+/* Name of package */
+#define PACKAGE "swirly"
+
+/* Version number of package */
+#define VERSION "1.0"
+
+/* C++ compiler supports template repository */
+#define HAVE_TEMPLATE_REPOSITORY 1
+
